e55aa2f7faa681958731fbf43dd3488e3a311f3f
Tyo <tyo16.id@gmail.com>
2021-05-24 23:16:26 +0300

    ✨ added cli support

---
+++ src/terminal.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Input } from "enquirer";
+import { executeBuildTest } from "./main";
+
+const prompt = new Input({
+  message: "Whats the directory folder you want to check?",
+});
+
+prompt
+  .run()
+  .then((answer: string) => {
+    console.log("Your output:", answer.trim());
+    executeBuildTest(answer.trim());
+  })
+  .catch(console.log);
