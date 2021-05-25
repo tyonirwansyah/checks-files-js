7887d9e76483ed7257ea1ad83b53e748c4f5bc97
Tyo <tyo16.id@gmail.com>
2021-05-25 22:48:43 +0300

    ✨ added two command methods

--- bin/terminal.js
+++ bin/terminal.js
@@ -1,18 +1,33 @@
 #!/usr/bin/env node
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const enquirer_1 = require("enquirer");
 const main_1 = require("./main");
+const yargs_1 = __importDefault(require("yargs"));
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const argv = yargs_1.default.argv;
 const prompt = new enquirer_1.Input({
     message: "Whats the directory folder you want to check?",
 });
-prompt
-    .run()
-    .then((answer) => {
-    console.log("Your output:", answer.trim());
-    main_1.executeBuildTest(answer.trim());
-})
-    .catch((e) => console.log(e));
+const commandInput = argv._[0];
+if (commandInput) {
+    main_1.executeBuildTest(commandInput.trim());
+}
+else {
+    runPrompt();
+}
+function runPrompt() {
+    prompt
+        .run()
+        .then((answer) => {
+        console.log("Your output:", answer.trim());
+        main_1.executeBuildTest(answer.trim());
+    })
+        .catch((e) => console.log(e));
+}
 //# sourceMappingURL=terminal.js.map
--- src/terminal.ts
+++ src/terminal.ts
@@ -2,17 +2,32 @@
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
+
 import { Input } from "enquirer";
 import { executeBuildTest } from "./main";
+import yargs from "yargs";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const argv: any = yargs.argv;
 
 const prompt = new Input({
   message: "Whats the directory folder you want to check?",
 });
 
-prompt
-  .run()
-  .then((answer: string) => {
-    console.log("Your output:", answer.trim());
-    executeBuildTest(answer.trim());
-  })
-  .catch((e: Error) => console.log(e));
+const commandInput = argv._[0];
+
+if (commandInput) {
+  executeBuildTest(commandInput.trim());
+} else {
+  runPrompt();
+}
+
+function runPrompt() {
+  prompt
+    .run()
+    .then((answer: string) => {
+      console.log("Your output:", answer.trim());
+      executeBuildTest(answer.trim());
+    })
+    .catch((e: Error) => console.log(e));
+}
